18f2bdcd77f4aa5a9c6ba4e2baffd5f5d2271e66
Dipanshu Singh <ds23322seven@gmail.com>
2026-03-30 12:30:44 +0000

    Navbar Slider

--- Navbar Slider/script.js
+++ Navbar Slider/script.js
@@ -1,5 +1,27 @@
 const menuEl = document.getElementById("Menu");
-const sideEl = document.querySelector("SideBar");
+const sideEl = document.querySelector(".SideBar");
+
+let isOpen = false;
+
 menuEl.addEventListener("click", function () {
-  sideEl.classList.add("AddId");
+  isOpen = !isOpen;
+  if (menuEl.src.endsWith("1828859.png")) {
+    menuEl.style.width = "80px";
+    console.log("hi");
+  } else {
+    menuEl.style.width = "40px";
+  }
+  if (isOpen) {
+    sideEl.classList.add("AddId");
+    menuEl.setAttribute(
+      "src",
+      "https://cdn-icons-png.flaticon.com/128/2319/2319179.png",
+    );
+  } else {
+    sideEl.classList.remove("AddId");
+    menuEl.setAttribute(
+      "src",
+      "https://cdn-icons-png.flaticon.com/128/1828/1828859.png",
+    );
+  }
 });
