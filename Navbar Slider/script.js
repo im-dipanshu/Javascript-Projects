2113a27d47c9afbba73dfcd560ffebb3862ef496
Dipanshu Singh <ds23322seven@gmail.com>
2026-03-30 11:53:50 +0000

    Menu

---
+++ Navbar Slider/script.js
@@ -0,0 +1,5 @@
+const menuEl = document.getElementById("Menu");
+const sideEl = document.querySelector("SideBar");
+menuEl.addEventListener("click", function () {
+  sideEl.classList.add("AddId");
+});
