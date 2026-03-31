8a5c0d4988adf679da91b51fce086cf74301d942
Dipanshu Singh <ds23322seven@gmail.com>
2026-03-31 05:09:31 +0000

    Navbar Functionality

--- Navbar Slider/script.js
+++ Navbar Slider/script.js
@@ -1,11 +1,18 @@
 const menuEl = document.getElementById("Menu");
 const sideEl = document.querySelector(".SideBar");
 
+const searchboxEl = document.getElementById("searchBox");
+const searchbuttonEl = document.getElementById("searchButton");
+const h1El = document.getElementById("H1");
+const h2El = document.getElementById("H2");
+const h3El = document.getElementById("H3");
+const seacrhdivEl = document.getElementById("Search");
+const paragraphEl = document.querySelectorAll("#contentPara");
 let isOpen = false;
 
 menuEl.addEventListener("click", function () {
   isOpen = !isOpen;
-  if (menuEl.src.endsWith("1828859.png")) {
+  if (menuEl.src.endsWith("12314149.png")) {
     menuEl.style.width = "80px";
     console.log("hi");
   } else {
@@ -21,7 +28,32 @@ menuEl.addEventListener("click", function () {
     sideEl.classList.remove("AddId");
     menuEl.setAttribute(
       "src",
-      "https://cdn-icons-png.flaticon.com/128/1828/1828859.png",
+      "https://cdn-icons-png.flaticon.com/128/12314/12314149.png",
     );
+  }
+});
+
+searchbuttonEl.addEventListener("click", function () {
+  const searchText = searchboxEl.value.trim().toLowerCase();
+
+  const headingText1 = h1El.textContent.trim().toLowerCase();
+  const headingText2 = h2El.textContent.trim().toLowerCase();
+  const headingText3 = h3El.textContent.trim().toLowerCase();
+
+  if (headingText2.includes(searchText)) {
+    seacrhdivEl.prepend(h2El);
+  }
+  if (headingText1.includes(searchText)) {
+    seacrhdivEl.prepend(h1El);
+  }
+  if (headingText3.includes(searchText)) {
+    seacrhdivEl.prepend(h3El);
+  }
+
+  for (i = 0; i <= paragraphEl.length; i++) {
+    const para = paragraphEl[i].textContent.trim().toLowerCase();
+    if (para.includes(searchText)) {
+      seacrhdivEl.prepend(paragraphEl[i]);
+    }
   }
 });
