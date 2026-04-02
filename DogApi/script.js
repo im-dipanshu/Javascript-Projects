be061f5df1634c969183966f4473441481af724c
Dipanshu Singh <ds23322seven@gmail.com>
2026-04-02 09:42:39 +0000

    Promises

--- DogApi/script.js
+++ DogApi/script.js
@@ -1,6 +1,22 @@
 async function getActivity() {
   try {
-    const response = await fetch("https://apis.scrimba.com/bored/api/activity");
+    const response = await fetch(
+      "https://apis.scrimba.com/jsonplaceholder/posts",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title: "Holiday Nightmares",
+          body: "When I was kidnapped in scotland",
+          userId: 2,
+        }),
+      },
+    );
+    if (!response.ok) {
+      throw new Error("There was a problem with the API");
+    }
     const data = await response.json();
     console.log(data);
   } catch (err) {
@@ -11,3 +27,51 @@ async function getActivity() {
 }
 
 getActivity();
+
+// const promise = new Promise((resolve, reject) => {
+//   const success = Math.random() > 0.5;
+//   if (success) {
+//     resolve("Operation Successfful!");
+//   } else {
+//     reject("Operation Rejected");
+//   }
+// });
+
+// promise.then((response) => {
+//   console.log(response);
+// });
+
+// async function Check(promise) {
+//   try {
+//   const response = await promise;
+//   console.log(response);
+// } catch (err) {
+//   console.log(err);
+// }
+// }
+
+// Check(promise)
+
+function preLoadImg(url) {
+  return new Promise((resolve, reject) => {
+    const image = new Image();
+    image.src = url;
+    image.addEventListener("load", () => resolve(image));
+    image.addEventListener("error", () =>
+      reject(`Image failed to load: ${url}`)
+    );
+  });
+}
+
+async function Check() {
+  try {
+    const result = await preLoadImg(
+      "https://scrimba.ams3.cdn.digitaloceanspaces.com/assets/courses/gadvancedjs/scenic1.jpg"
+    );
+    console.log(result);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+Check();
